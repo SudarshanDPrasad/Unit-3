ffc98866fc9371741abd83ddcba83ae4af4c14ee
sudararshan <sudarshanprasad18@gmail.com>
2021-07-30 00:48:24 +0530

    Switch App2

---
+++ BroadCastReciever/Send Broadcast between Apps1/app/src/main/java/com/example/sendbroadcastbetweenapps/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.sendbroadcastbetweenapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSend = findViewById(R.id.btnSend);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent("Masai.switch.apps");
+                sendBroadcast(intent);
+
+            }
+        });
+    }
+}
