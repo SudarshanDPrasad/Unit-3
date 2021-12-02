bfc2ff1e211332a01e4ef265fa68e691683c9374
Sudarshan Prasad <86509887+SudarshanDPrasad@users.noreply.github.com>
2021-12-02 18:57:29 +0530

    Delete CodingTask/iTunes Song directory

--- CodingTask/iTunes Song/app/src/main/java/com/application/itunessong/data/MainUiModel.kt
+++
@@ -1,9 +0,0 @@
-package com.application.itunessong.data
-
-import com.application.itunessong.responsemodel.ResponseDTO
-
-sealed class MainUiModel {
-
-    data class OnSuccess(val responseDTO: ResponseDTO) : MainUiModel()
-    data class OnFailure(val error: String) : MainUiModel()
-}
